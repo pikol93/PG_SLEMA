bc99baf3626d8ba20cb9e5854b90c28da20bba4f
Sladamos <sladamo@wp.pl>
2023-11-09 22:38:36 +0100

    SLEMA-39 edit plan widget positioned properly

--- lib/features/exercises/presentation/widget/exercises_screen.dart
+++ lib/features/exercises/presentation/widget/exercises_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pg_slema/features/exercises/presentation/widget/exercise_widget.dart';
 
 class ExercisesScreen extends StatefulWidget {
   const ExercisesScreen({super.key});
@@ -11,102 +12,32 @@ class ExercisesScreenState extends State<ExercisesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
+      body: Stack(
         children: [
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            color: Colors.red,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Podciąganie',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 8.0),
-                const Text(
-                  'Powtórzenia: 3x8',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-                const Text(
-                  'Obciążenie: +15kg',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 8.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Seria 1/5',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Container(
-                      width: 30,
-                      height: 30,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle,
-                      ),
-                      child: const Center(
-                        child: Text(
-                          '+',
-                          style: TextStyle(fontSize: 16.0, color: Colors.red),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-
-
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Akcja po naciśnięciu przycisku "Edytuj ćwiczenie"
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.brown,
-                shape: const StadiumBorder(),
+          ListView(children: const [
+            ExerciseWidget(
+                exerciseName: "Podciąganie",
+                series: "1/3",
+                repetitions: "5",
+                weight: "-15kg"),
+          ]),
+          Positioned(
+            bottom: 20,
+            right: 30,
+            child: Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              padding: const EdgeInsets.all(16.0),
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
               ),
-              child: const Text(
-                'Edytuj ćwiczenie',
-                style: TextStyle(color: Colors.white),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.edit_calendar, color: Colors.white),
+                ],
               ),
             ),
-
-          ),
-          Align(
-              alignment: Alignment.bottomRight,
-              child: Container(
-            margin: const EdgeInsets.only(top: 16.0),
-            padding: const EdgeInsets.all(16.0),
-            decoration: const BoxDecoration(
-              color: Colors.red,
-              shape: BoxShape.circle,
-            ),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(Icons.edit_calendar, color: Colors.white),
-              ],
-            ),
-          ),
           ),
         ],
       ),
