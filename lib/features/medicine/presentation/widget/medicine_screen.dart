cba8103bf4969c04511a4c255f9183e867d94933
amddaa <slusarekadam1@gmail.com>
2023-11-11 12:27:38 +0100

    SLEMA-12 Add medicine feature screen

---
+++ lib/features/medicine/presentation/widget/medicine_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class DietScreen extends StatelessWidget {
+  const DietScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Diet Screen'),
+    );
+  }
+}
