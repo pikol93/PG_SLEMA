c8533aafd3385b3fb56bd17d6e6287c1f0d481b2
amddaa <slusarekadam1@gmail.com>
2023-11-11 14:42:20 +0100

    SLEMA-12 Add medicine calendar placeholder

---
+++ lib/features/medicine/presentation/widget/medicine_calendar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MedicineCalendar extends StatelessWidget {
+  const MedicineCalendar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text('Miejsce na kalendarz/overview tygodnia z wybranym dniem'
+          '/data?\nWtedy zmieniajc dzien moglibysmy zobaczyc co musimy zażyć,'
+          ' dodac nowy lek lub go edytować. Moglibysmy tez "odhaczać" już zażyte'
+          ' leki.'),
+    );
+  }
+}
--- lib/features/medicine/presentation/widget/medicine_screen.dart
+++ lib/features/medicine/presentation/widget/medicine_screen.dart
@@ -9,6 +9,15 @@ class MedicineScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: const Column(
+        children: [
+          MedicineCalendar(),
+          SizedBox(height: 20.0),
+          Center(
+            child: Text("Medicine screen"),
+          ),
+        ],
+      ),
       floatingActionButton: AddMedicineButton(onPressed: () {
         //TODO Stworzyc kontroler i dodac tutaj obsluge tworzenia leku
       }),
