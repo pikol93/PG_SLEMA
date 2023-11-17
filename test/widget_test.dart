f9cab157a711ddf1c9ac30dbf9bbb2ee5d451678
pikol93 <pikol93@wp.pl>
2023-11-17 16:42:01 +0100

    SLEMA-43 Added a temporary test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test("should succeed", () {
+    // This test is placed here for the build process to succeed and should be
+    // removed as soon as any valid test shows up.
+  });
+}
