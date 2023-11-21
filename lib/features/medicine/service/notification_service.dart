1ff5056cc548be632391bf3097681d76718856a0
Sladamos <sladamo@wp.pl>
2023-11-21 17:02:08 +0100

    SLEMA-13 possibility to show notifications without scheduling

---
+++ lib/features/medicine/service/notification_service.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:async';
+
+class NotificationService {
+  static final FlutterLocalNotificationsPlugin
+      _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  Future<void> initNotifications() async {
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    var initializationSettingsIOS = DarwinInitializationSettings(
+        requestAlertPermission: true,
+        requestBadgePermission: true,
+        requestSoundPermission: true,
+        onDidReceiveLocalNotification:
+            (int id, String? title, String? body, String? payload) async {});
+
+    var initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse:
+            (NotificationResponse notificationResponse) async {});
+  }
+
+  notificationDetails() {
+    return const NotificationDetails(
+        android: AndroidNotificationDetails('channelId', 'channelName',
+            importance: Importance.max),
+        iOS: DarwinNotificationDetails());
+  }
+
+  Future showNotification(
+      {int id = 0, String? title, String? body, String? payload}) async {
+    return _flutterLocalNotificationsPlugin.show(
+        id, title, body, await notificationDetails());
+  }
+}
