1508d4eb629cce640be5edd523e96d6ecbe4616b
pikol93 <pikol93@wp.pl>
2023-11-29 15:05:53 +0100

    SLEMA-47 Added logger_mixin.dart

---
+++ lib/utils/log/logger_mixin.dart
@@ -0,0 +1,9 @@
+import 'package:loggy/loggy.dart';
+
+mixin Logger implements LoggyType {
+  Loggy<Logger> get logger => loggy;
+
+  @override
+  Loggy<Logger> get loggy =>
+      Loggy<Logger>(runtimeType.toString());
+}
