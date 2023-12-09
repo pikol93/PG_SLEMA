a6417bf8909e2277679b1ef471bc40ddf8c26c2a
amddaa <slusarekadam1@gmail.com>
2023-12-09 23:17:00 +0100

    SLEMA-66 Separate diet AppBar

--- lib/features/diet/presentation/widget/diet_screen.dart
+++ lib/features/diet/presentation/widget/diet_screen.dart
@@ -1,70 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:pg_slema/features/diet/presentation/controller/calendar_controller.dart';
-import 'package:pg_slema/features/diet/presentation/widget/calendar_exact_date_picker.dart';
-import 'package:pg_slema/features/diet/presentation/widget/calendar_move_backward.dart';
-import 'package:pg_slema/features/diet/presentation/widget/calendar_move_forward.dart';
+import 'package:pg_slema/features/diet/presentation/widget/diet_app_bar.dart';
 
 class DietScreen extends StatefulWidget {
-  final CalendarController exactDatePickerController = CalendarController();
-  DietScreen({super.key});
+  const DietScreen({super.key});
 
   @override
   State<DietScreen> createState() => _DietScreenState();
 }
 
-class _DietScreenState extends State<DietScreen>
-    with AutomaticKeepAliveClientMixin {
-  String appBarDateText = "Dzisiaj";
-  void updatePickedDateStringRepresentation(String value) {
-    setState(() {
-      appBarDateText = value;
-    });
-  }
-
+class _DietScreenState extends State<DietScreen> {
   @override
   Widget build(BuildContext context) {
-    super.build(context);
     return Scaffold(
-      appBar: AppBar(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.elliptical(15, 10),
-          ),
-        ),
-        elevation: 5,
-        toolbarHeight: 60,
-        title: Row(
-          children: [
-            CalendarExactDatePicker(
-              onPickedDate: updatePickedDateStringRepresentation,
-              controller: widget.exactDatePickerController,
-            ),
-            const Spacer(),
-            CalendarMoveBackward(
-              onPickedDate: updatePickedDateStringRepresentation,
-              controller: widget.exactDatePickerController,
-            ),
-            Text(
-              appBarDateText,
-              style: const TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            CalendarMoveForward(
-              onPickedDate: updatePickedDateStringRepresentation,
-              controller: widget.exactDatePickerController,
-            ),
-            const Spacer(flex: 4),
-          ],
-        ),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: DietAppBar(),
       ),
       body: const Center(
         child: Text("TODO"),
       ),
     );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
