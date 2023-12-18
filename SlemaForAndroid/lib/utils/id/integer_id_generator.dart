0d3afd7fc2fda061c601fbbb7ab416edce890ef3
Sladamos <sladamo@wp.pl>
2023-12-18 20:08:03 +0100

    SLEMA-65 upgraded id generator with new methods

--- SlemaForAndroid/lib/utils/id/integer_id_generator.dart
+++ SlemaForAndroid/lib/utils/id/integer_id_generator.dart
@@ -8,6 +8,22 @@ class IntegerIdGenerator {
     return random.nextInt(maxInt);
   }
 
+  static List<int> generateRandomIds(int size) {
+    return List<int>.generate(size, (index) => generateRandomId());
+  }
+
+  static List<int> generateRandomIdsWhichAreNotForbidden(List<int> forbiddenIds, int size) {
+    List<int> ids = List<int>.empty(growable: true);
+    while (size > 0) {
+      int id = generateRandomIdWhichIsNotForbidden(forbiddenIds);
+      if(!ids.contains(id)) {
+        ids.add(id);
+        size--;
+      }
+    }
+    return ids;
+  }
+
   static int generateRandomIdWhichIsNotForbidden(List<int> forbiddenIds) {
     Random random = Random();
     int randomId;
