aa4a1140253ac1766db3fc87226fd081c6bcbca4
Sladamos <sladamo@wp.pl>
2023-12-26 18:30:02 +0100

    SLEMA-65 properly creating new notifications

--- SlemaForAndroid/lib/features/notification/presentation/widget/manage_notifications_widget.dart
+++ SlemaForAndroid/lib/features/notification/presentation/widget/manage_notifications_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pg_slema/features/notification/domain/get_notification.dart';
 import 'package:pg_slema/features/notification/presentation/controller/get_notification_controller.dart';
 import 'package:pg_slema/features/notification/presentation/controller/manage_notifications_controller.dart';
+import 'package:pg_slema/features/notification/presentation/widget/create_notification_button.dart';
 import 'package:pg_slema/features/notification/presentation/widget/get_notification_widget.dart';
 
 class ManageNotificationsWidget extends StatefulWidget {
@@ -17,25 +18,36 @@ class ManageNotificationsWidget extends StatefulWidget {
 class _ManageNotificationsWidgetState extends State<ManageNotificationsWidget> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement button to add new(created)
-    return ListView.builder(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemCount: widget.controller.notifications.length,
-      itemBuilder: (context, index) {
-        final notification = widget.controller.notifications[index];
-        return GetNotificationWidget(
-          onNotificationChanged:
-              widget.controller.onNotificationChanged, //update?
-          onNotificationDeleted: onNotificationDeleted,
-          controller: GetNotificationController(notification: notification),
-        );
-      },
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Flexible(
+            child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: widget.controller.notifications.length,
+          itemBuilder: (context, index) {
+            final notification = widget.controller.notifications[index];
+            return GetNotificationWidget(
+              onNotificationChanged:
+                  widget.controller.onNotificationChanged, //update?
+              onNotificationDeleted: onNotificationDeleted,
+              controller: GetNotificationController(notification: notification),
+            );
+          },
+        )),
+        CreateNotificationButton(onNotificationCreated: onNotificationCreated)
+      ],
     );
   }
 
   void onNotificationDeleted(GetNotification notification) {
     widget.controller.onNotificationDeleted(notification);
+    setState(() {});
+  }
+
+  void onNotificationCreated(GetNotification notification) {
+    widget.controller.onNotificationCreated(notification);
     setState(() {});
   }
 }
