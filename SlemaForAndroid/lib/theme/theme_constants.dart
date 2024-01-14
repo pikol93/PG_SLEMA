6cf579bcc8877b2f7743301a2ff91d4cf74a4493
amddaa <slusarekadam1@gmail.com>
2024-01-14 12:22:48 +0100

    SLEMA-77 Add first theme for NavigationBar widget

--- SlemaForAndroid/lib/theme/theme_constants.dart
+++ SlemaForAndroid/lib/theme/theme_constants.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
+const colorSchemeSeed = Color(0xFF1E81B0);
+
+//NavigationBar
+const colorNavigationBarBackground = Color(0xFF6793B3);
+const colorNavigationBarShadow = Color(0x1A000000);
+
 ThemeData lightTheme = ThemeData(
-  useMaterial3: true,
-  brightness: Brightness.light,
-  colorSchemeSeed: const Color(0xffb6ddfa),
-);
+    useMaterial3: true,
+    brightness: Brightness.light,
+    colorSchemeSeed: colorSchemeSeed,
+    navigationBarTheme: const NavigationBarThemeData(
+      backgroundColor: colorNavigationBarBackground,
+      elevation: 0,
+      height: 72,
+      shadowColor: colorNavigationBarShadow,
+    ));
 
 ThemeData darkTheme =
     ThemeData(useMaterial3: true, brightness: Brightness.dark);
