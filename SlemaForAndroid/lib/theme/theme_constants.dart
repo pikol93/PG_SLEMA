e721dcdfdeeb37871d6b6654eb1f3d2d2f103e32
amddaa <slusarekadam1@gmail.com>
2024-01-14 11:51:25 +0100

    SLEMA-77 Create basic theme constants for light and dark theme

---
+++ SlemaForAndroid/lib/theme/theme_constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.light,
+  colorSchemeSeed: const Color(0xffb6ddfa),
+);
+
+ThemeData darkTheme =
+    ThemeData(useMaterial3: true, brightness: Brightness.dark);
