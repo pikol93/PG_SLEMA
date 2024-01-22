5cd74f05fa40b0816579b28a97acdd640886f8fa
amddaa <slusarekadam1@gmail.com>
2024-01-23 00:39:37 +0100

    SLEMA-96 Add styling for save button

--- SlemaForAndroid/lib/features/medicine/presentation/widget/formWidgets/save_button.dart
+++ SlemaForAndroid/lib/features/medicine/presentation/widget/formWidgets/save_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pg_slema/features/medicine/presentation/controller/add_medicine_controller.dart';
+import 'package:pg_slema/theme/theme_constants.dart';
 
 class CustomSaveButton extends StatelessWidget {
   final AddMedicineController controller;
@@ -15,27 +16,39 @@ class CustomSaveButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
-      height: 50.0,
-      child: ElevatedButton(
-        onPressed: () {
-          if (formKey.currentState!.validate()) {
-            try {
-              addNewMedicine(context);
-            } on Exception catch (_) {
-              showInfoAboutIncorrectSave(context);
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: ElevatedButton(
+          onPressed: () {
+            if (formKey.currentState!.validate()) {
+              try {
+                addNewMedicine(context);
+              } on Exception catch (_) {
+                showInfoAboutIncorrectSave(context);
+              }
             }
-          }
-        },
-        style: ButtonStyle(
-          backgroundColor:
-              MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
-        ),
-        child: const Text(
-          "Zapisz",
-          style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.white,
+          },
+          style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0))),
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(colorCustomSaveButton),
+              elevation: MaterialStateProperty.all(6)),
+          child: const Padding(
+            padding: EdgeInsets.all(14.0),
+            child: Center(
+              child: Text(
+                "Zapisz",
+                style: TextStyle(
+                  fontFamily: 'Dongle',
+                  fontSize: 46,
+                  height: 0.52381,
+                  color: colorAppBarText,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
           ),
         ),
       ),
--- SlemaForAndroid/lib/theme/theme_constants.dart
+++ SlemaForAndroid/lib/theme/theme_constants.dart
@@ -10,45 +10,49 @@ const colorNavigationBarShadow = Color(0x1A000000);
 const colorAppBarText = Color(0xFFF2F1EB);
 const colorAppBarTextShadow = Color(0x40000000);
 
+//CustomSaveButton
+const colorCustomSaveButton = Color(0xFF487ABC);
+
 ThemeData lightTheme = ThemeData(
-    fontFamily: 'Dongle',
-    useMaterial3: true,
-    brightness: Brightness.light,
-    colorSchemeSeed: colorSchemeSeed,
-    navigationBarTheme: const NavigationBarThemeData(
-      backgroundColor: colorNavigationBarBackground,
-      elevation: 0,
-      height: 72,
-      shadowColor: colorNavigationBarShadow,
+  fontFamily: 'Dongle',
+  useMaterial3: true,
+  brightness: Brightness.light,
+  colorSchemeSeed: colorSchemeSeed,
+  navigationBarTheme: const NavigationBarThemeData(
+    backgroundColor: colorNavigationBarBackground,
+    elevation: 0,
+    height: 72,
+    shadowColor: colorNavigationBarShadow,
+  ),
+  appBarTheme: const AppBarTheme(
+    titleTextStyle: TextStyle(
+      fontFamily: 'Dongle',
+      fontWeight: FontWeight.w700,
+      fontSize: 50,
+      height: 0.52381,
+      color: colorAppBarText,
+      shadows: [
+        Shadow(
+          color: colorAppBarTextShadow,
+          offset: Offset(0, 4),
+          blurRadius: 4,
+        ),
+      ],
     ),
-    appBarTheme: const AppBarTheme(
-      titleTextStyle: TextStyle(
-        fontFamily: 'Dongle',
-        fontWeight: FontWeight.w700,
-        fontSize: 50,
-        height: 0.52381,
-        color: colorAppBarText,
-        shadows: [
-          Shadow(
-            color: colorAppBarTextShadow,
-            offset: Offset(0, 4),
-            blurRadius: 4,
-          ),
-        ],
-      ),
-      iconTheme: IconThemeData(
-        weight: 700,
-        color: colorAppBarText,
-        size: 36,
-        shadows: [
-          Shadow(
-            color: colorAppBarTextShadow,
-            offset: Offset(0, 4),
-            blurRadius: 4,
-          ),
-        ],
-      ),
-    ));
+    iconTheme: IconThemeData(
+      weight: 700,
+      color: colorAppBarText,
+      size: 36,
+      shadows: [
+        Shadow(
+          color: colorAppBarTextShadow,
+          offset: Offset(0, 4),
+          blurRadius: 4,
+        ),
+      ],
+    ),
+  ),
+);
 
 ThemeData darkTheme =
     ThemeData(useMaterial3: true, brightness: Brightness.dark);
