6f399669f292f34097b0f302794c1e03362bf2a9
Sladamos <sladamo@wp.pl>
2024-01-31 00:03:55 +0100

    SLEMA-105 temporrary data init

--- SlemaForAndroid/lib/initializers/diet_initializer.dart
+++ SlemaForAndroid/lib/initializers/diet_initializer.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:pg_slema/features/dish/application/dish_service.dart';
 import 'package:pg_slema/features/dish/domain/dish.dart';
 import 'package:pg_slema/features/dish_category/application/dish_category_name.dart';
@@ -5,7 +7,12 @@ import 'package:pg_slema/features/dish_category/application/dish_category_servic
 import 'package:pg_slema/features/dish_category/data/repository/shared_preferences_dish_category_repository.dart';
 import 'package:pg_slema/features/dish_category/domain/converter/dish_category_to_dto.dart';
 import 'package:pg_slema/features/dish_category/domain/dish_category.dart';
+import 'package:pg_slema/features/meal/application/meal_service.dart';
+import 'package:pg_slema/features/meal/data/repository/shared_preferences_meal_repository.dart';
+import 'package:pg_slema/features/meal/domain/converter/meal_to_dto_converter.dart';
+import 'package:pg_slema/features/meal/domain/meal.dart';
 import 'package:pg_slema/initializers/initializer.dart';
+import 'package:pg_slema/utils/meal_time/meal_time.dart';
 import 'package:uuid/uuid.dart';
 
 class DietInitializer with Initializer {
@@ -36,6 +43,7 @@ class DietInitializer with Initializer {
       'Cielęcina',
       'Baranina'
     ]);
+    await initializeMeals(); //TODO: remove
   }
 
   Future initializeDishesForCategory(
@@ -65,5 +73,35 @@ class DietInitializer with Initializer {
     return names
         .map((e) => Dish(idGenerator.v4(), e, categoryId))
         .toList(growable: true);
+  }
+
+  Future initializeMeals() async {
+    if (false) {
+      var dishes = await dishService.getAllDishes();
+      var mealService = MealService(
+          SharedPreferencesMealRepository(MealToDtoConverter(dishService)));
+      await mealService.addMeal(Meal(
+          idGenerator.v4(), dishes[0], DateTime.now(), MealTime.firstMeal));
+      await mealService.addMeal(Meal(
+          idGenerator.v4(), dishes[1], DateTime.now(), MealTime.secondMeal));
+      await mealService.addMeal(Meal(
+          idGenerator.v4(), dishes[1], DateTime.now(), MealTime.secondMeal));
+      await mealService.addMeal(Meal(
+          idGenerator.v4(),
+          dishes[2],
+          DateTime.now().subtract(const Duration(days: 1)),
+          MealTime.thirdMeal));
+      await mealService.addMeal(Meal(
+          idGenerator.v4(),
+          dishes[3],
+          DateTime.now().subtract(const Duration(days: 1)),
+          MealTime.fourthMeal));
+      await mealService.addMeal(Meal(
+          idGenerator.v4(),
+          dishes[4],
+          DateTime.now().subtract(const Duration(days: 1)),
+          MealTime.fourthMeal));
+      var meals = await mealService.getAllMeals();
+    }
   }
 }
