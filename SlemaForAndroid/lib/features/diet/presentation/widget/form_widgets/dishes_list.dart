2b06e7a15fd4aff39956ce14df976dc53888f497
Sladamos <sladamo@wp.pl>
2024-02-18 23:59:48 +0100

    SLEMA-140 dishes in dish category widget

--- SlemaForAndroid/lib/features/diet/presentation/widget/form_widgets/dishes_list.dart
+++ SlemaForAndroid/lib/features/diet/presentation/widget/form_widgets/dishes_list.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pg_slema/features/dish/logic/entity/dish.dart';
 
-class DishesList extends StatefulWidget {
+class DishesList extends StatelessWidget {
   final List<Dish> dishes;
   final ValueChanged<Dish> onDishAdded;
   final ValueChanged<String> onDishRemoved;
@@ -15,28 +14,23 @@ class DishesList extends StatefulWidget {
       required this.onDishRemoved,
       required this.dishesToggles});
 
-  @override
-  State<StatefulWidget> createState() => _DishesListState();
-
   List<Text> dishesToWidgets() {
     return dishes.map((e) => Text(e.name)).toList();
   }
-}
 
-class _DishesListState extends State<DishesList> {
   @override
   Widget build(BuildContext context) {
     return ToggleButtons(
-        isSelected: widget.dishesToggles,
+        isSelected: dishesToggles,
         onPressed: onTogglePressed,
-        children: widget.dishesToWidgets());
+        children: dishesToWidgets());
   }
 
   void onTogglePressed(int index) {
-    if (widget.dishesToggles[index] == true) {
-      widget.onDishRemoved(widget.dishes[index].id);
+    if (dishesToggles[index] == true) {
+      onDishRemoved(dishes[index].id);
     } else {
-      widget.onDishAdded(widget.dishes[index]);
+      onDishAdded(dishes[index]);
     }
   }
 }
