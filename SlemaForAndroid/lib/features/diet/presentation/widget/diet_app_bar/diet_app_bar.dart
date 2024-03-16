28d06510880d4111ef9a957b61378eb1db11a870
amddaa <slusarekadam1@gmail.com>
2024-03-16 17:39:19 +0100

    SLEMA-138 Delete unused keep alive

--- SlemaForAndroid/lib/features/diet/presentation/widget/diet_app_bar/diet_app_bar.dart
+++ SlemaForAndroid/lib/features/diet/presentation/widget/diet_app_bar/diet_app_bar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pg_slema/features/diet/presentation/widget/diet_app_bar/calendar_with_arrows.dart';
 import 'package:pg_slema/features/diet/presentation/widget/diet_app_bar/meal_idea_icon_button.dart';
 
-class DietAppBar extends StatefulWidget {
+class DietAppBar extends StatefulWidget implements PreferredSizeWidget {
   final ValueChanged<DateTime> onDateChanged;
   final DateTime initDate;
 
@@ -11,28 +11,17 @@ class DietAppBar extends StatefulWidget {
 
   @override
   State<DietAppBar> createState() => _DietAppBarState();
-}
-
-class _DietAppBarState extends State<DietAppBar>
-    with AutomaticKeepAliveClientMixin {
-  String appBarDateText = "Dzisiaj";
-  void updatePickedDateStringRepresentation(String value) {
-    setState(() {
-      appBarDateText = value;
-    });
-  }
 
   @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _DietAppBarState extends State<DietAppBar> {
+  @override
   Widget build(BuildContext context) {
-    super.build(context);
     return AppBar(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.elliptical(15, 10),
-          ),
-        ),
-        elevation: 5,
-        toolbarHeight: 100,
+        toolbarHeight: kToolbarHeight + 10,
+        backgroundColor: Theme.of(context).primaryColor,
         title: Row(
           children: [
             Expanded(
