2dc3fff6d28d6ad619cd1638317b655e80deacea
amddaa <slusarekadam1@gmail.com>
2024-03-16 23:53:39 +0100

    SLEMA-138 Add meals containers

--- SlemaForAndroid/lib/features/diet/presentation/widget/meals_in_meal_time_widget.dart
+++ SlemaForAndroid/lib/features/diet/presentation/widget/meals_in_meal_time_widget.dart
@@ -1,39 +1,52 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pg_slema/utils/widgets/default_container/container_with_divider.dart';
 import 'package:pg_slema/features/meal/logic/entity/meal.dart';
 import 'package:pg_slema/features/meal/logic/entity/meal_time.dart';
+import 'package:pg_slema/features/diet/presentation/widget/form_widgets/ingredients_list.dart';
+import 'package:pg_slema/features/diet/presentation/screen/update_meals_screen.dart';
 
 class MealInMealTimeWidget extends StatelessWidget {
   final MealTime mealTime;
   final Meal? meal;
+  final ValueChanged<Map<MealTime, Meal>> onMealsSelected;
+  final ValueGetter<Map<MealTime, Meal>> initMealsProvider;
 
   const MealInMealTimeWidget(
-      {super.key, required this.mealTime, required this.meal});
+      {super.key,
+      required this.mealTime,
+      required this.meal,
+      required this.onMealsSelected,
+      required this.initMealsProvider});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          mealTime.name,
-        ),
-        Builder(
-          builder: (BuildContext context) {
-            return CustomScrollView(
-              shrinkWrap: true,
-              slivers: [
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                      return Text(meal?.ingredients.elementAt(index).name ?? "")
-                          .build(context);
-                    },
-                    childCount: meal?.ingredients.length ?? 0,
-                  ),
+        ContainerWithDivider(
+          label: mealTime.name,
+          iconDataRight: Icons.edit,
+          onIconRightPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => UpdateMealsScreen(
+                  onMealsUpdated: onMealsSelected,
+                  initMealsProvider: initMealsProvider,
                 ),
-              ],
+              ),
             );
           },
-        )
+          child: meal != null
+              ? IngredientsList(
+                  ingredients: meal!.ingredients,
+                  onIngredientAdded: null,
+                  onIngredientRemoved: null,
+                  ingredientsToggles: null,
+                  isExpanded: true,
+                  expandContainer: () {},
+                )
+              : null,
+        ),
       ],
     );
   }
