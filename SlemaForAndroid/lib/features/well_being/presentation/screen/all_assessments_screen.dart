0ff0e1180b577862effb52a2121edb685a4c0841
pikol93 <pikol93@wp.pl>
2024-03-22 21:23:21 +0100

    SLEMA-97 Have AllAssessmentsScreen dependencies be injected

--- SlemaForAndroid/lib/features/well_being/presentation/screen/all_assessments_screen.dart
+++ SlemaForAndroid/lib/features/well_being/presentation/screen/all_assessments_screen.dart
@@ -1,31 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:pg_slema/features/well_being/logic/entity/assessment.dart';
-import 'package:pg_slema/features/well_being/logic/repository/shared_preferences_assessments_repository.dart';
 import 'package:pg_slema/features/well_being/logic/service/assessments_service.dart';
-import 'package:pg_slema/features/well_being/logic/service/assessments_service_impl.dart';
 import 'package:pg_slema/features/well_being/presentation/widget/all_assessments/single_assessment_widget.dart';
 import 'package:pg_slema/utils/widgets/default_appbar/default_appbar.dart';
 import 'package:pg_slema/utils/widgets/default_body/default_body.dart';
 
 class AllAssessmentsScreen extends StatefulWidget {
-  const AllAssessmentsScreen({super.key});
+  final AssessmentsService service;
+
+  const AllAssessmentsScreen({super.key, required this.service});
 
   @override
   State<AllAssessmentsScreen> createState() => _AllAssessmentsScreenState();
 }
 
 class _AllAssessmentsScreenState extends State<AllAssessmentsScreen> {
-  late final AssessmentsService _service;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // TODO: Optimally, these should be injected.
-    var repository = SharedPreferencesAssessmentsRepository();
-    _service = AssessmentsServiceImpl(repository: repository);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
