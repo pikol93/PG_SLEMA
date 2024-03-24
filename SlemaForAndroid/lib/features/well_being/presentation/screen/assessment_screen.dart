d75a661218d3a26d90db581f6e4a8f418e65205f
pikol93 <pikol93@wp.pl>
2024-03-24 16:56:09 +0100

    SLEMA-97 Modified AssessmentScreen to use DefaultAppBar

--- SlemaForAndroid/lib/features/well_being/presentation/screen/assessment_screen.dart
+++ SlemaForAndroid/lib/features/well_being/presentation/screen/assessment_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:pg_slema/features/well_being/presentation/widget/assessment_app_bar.dart';
+import 'package:pg_slema/features/well_being/presentation/screen/add_assessment_screen_progress_bar.dart';
 import 'package:pg_slema/features/well_being/presentation/widget/forms/sleep/assessment_sleep_form.dart';
-import 'package:pg_slema/utils/widgets/default_container/default_container.dart';
 import 'package:pg_slema/features/well_being/presentation/widget/forms/symptoms/assessment_symptoms_form.dart';
 import 'package:pg_slema/features/well_being/presentation/widget/forms/well_being/assessment_well_being_form.dart';
-import 'package:pg_slema/utils/date/date.dart';
 import 'package:pg_slema/utils/log/logger_mixin.dart';
+import 'package:pg_slema/utils/widgets/default_appbar/default_appbar.dart';
+import 'package:pg_slema/utils/widgets/default_body/default_body.dart';
+import 'package:pg_slema/utils/widgets/default_container/default_container.dart';
 
 class AssessmentScreen extends StatefulWidget {
   const AssessmentScreen({super.key});
@@ -19,31 +20,34 @@ class _AssessmentScreenState extends State<AssessmentScreen> with Logger {
 
   @override
   Widget build(BuildContext context) {
-    final currentDateString = DateTime.now().toDateString();
-    return CustomScrollView(
-      slivers: <Widget>[
-        const AssessmentAppBar(),
-        SliverToBoxAdapter(
-          child: Form(
-            key: _formKey,
-            child: const Column(
-              children: [
-                DefaultContainer(
-                  padding: EdgeInsets.all(15),
-                  child: AssessmentWellBeingForm(),
-                ),
-                DefaultContainer(
-                  padding: EdgeInsets.all(15),
-                  child: AssessmentSymptomsFormWidget(),
-                ),
-                DefaultContainer(
-                  padding: EdgeInsets.all(15),
-                  child: AssessmentSleepForm(),
-                ),
-              ],
+    return Column(
+      children: [
+        const DefaultAppBar(
+          title: "Dodaj raport",
+        ),
+        DefaultBody(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: const Column(
+                children: [
+                  DefaultContainer(
+                    padding: EdgeInsets.all(15),
+                    child: AssessmentWellBeingForm(),
+                  ),
+                  DefaultContainer(
+                    padding: EdgeInsets.all(15),
+                    child: AssessmentSymptomsFormWidget(),
+                  ),
+                  DefaultContainer(
+                    padding: EdgeInsets.all(15),
+                    child: AssessmentSleepForm(),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
+        )
       ],
     );
   }
