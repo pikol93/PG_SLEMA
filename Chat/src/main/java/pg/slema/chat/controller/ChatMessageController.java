e1ad467b0fd3a8a7e85b483dab32e403972d490b
Sladamos <sladamo@wp.pl>
2024-04-02 15:35:52 +0200

    SLEMA-129 very simple chat with small message and without working JS

---
+++ Chat/src/main/java/pg/slema/chat/controller/ChatMessageController.java
@@ -0,0 +1,16 @@
+package pg.slema.chat.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import pg.slema.chat.dto.ChatMessage;
+
+@Controller
+public class ChatMessageController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public ChatMessage get(ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
---
+++ Chat/src/main/java/pg/slema/chat/dto/ChatMessage.java
@@ -0,0 +1,11 @@
+package pg.slema.chat.dto;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+
+    private String value;
+
+    private String user;
+}
