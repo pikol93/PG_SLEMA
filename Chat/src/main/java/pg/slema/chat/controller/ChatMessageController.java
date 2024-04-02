299d9edeb0725c9bf1408ce7ee20bef330dc044d
Sladamos <sladamo@wp.pl>
2024-04-02 22:58:53 +0200

    SLEMA-129 properly handling room id

--- Chat/src/main/java/pg/slema/chat/controller/ChatMessageController.java
+++ Chat/src/main/java/pg/slema/chat/controller/ChatMessageController.java
@@ -1,20 +1,30 @@
 package pg.slema.chat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import pg.slema.chat.dto.ChatMessage;
+
+import java.util.UUID;
 
 @Controller
 public class ChatMessageController {
 
-    @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public ChatMessage get(@Payload ChatMessage chatMessage) {
-        System.out.println(chatMessage.toString());
-        return chatMessage;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public ChatMessageController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/chat/{conversationId}")
+    public void handleChatMessage(@Payload ChatMessage chatMessage, @DestinationVariable String conversationId) {
+        System.out.println(chatMessage.toString() + "\n" + conversationId);
+        messagingTemplate.convertAndSend("/topic/messages/" + conversationId, chatMessage);
     }
 }
--- ChatJs/script.js
+++ ChatJs/script.js
@@ -1,28 +1,43 @@
 
 // Połączenie z serwerem WebSocket
-var client = new StompJs.Client({
-    brokerURL: 'ws://localhost:8081/chat-register',
-    reconnectDelay: 5000,
-    heartbeatIncoming: 4000,
-    heartbeatOutgoing: 4000,
-    onConnect: () => client.subscribe('/topic/messages', message => {
-        console.log(message);
-        var receivedMessage = JSON.parse(message.body);
-        console.log('Otrzymano wiadomość:', receivedMessage);
-        var messageDiv = document.createElement('div');
-        messageDiv.textContent = receivedMessage.user + ": " + receivedMessage.value;
-        document.getElementById('message-container').appendChild(messageDiv);
-      })
-})
 
-function sendMessage() {
+var client;
+var userName;
+var conversationId;
+
+
+function connect(roomId) {
+    var client = new StompJs.Client({
+        brokerURL: 'ws://localhost:8081/chat-register',
+        reconnectDelay: 5000,
+        heartbeatIncoming: 4000,
+        heartbeatOutgoing: 4000,
+        onConnect: () => client.subscribe(`/topic/messages/${roomId}`, message => {
+            console.log(message);
+            var receivedMessage = JSON.parse(message.body);
+            console.log('Otrzymano wiadomość:', receivedMessage);
+            var messageDiv = document.createElement('div');
+            messageDiv.textContent = receivedMessage.user + ": " + receivedMessage.value;
+            document.getElementById('message-container').appendChild(messageDiv);
+          })
+    })
+    var userName = document.getElementById('user-name').value;
+    client.activate();
+    return {client: client, userName: userName}
+}
+
+function disconnect(client) {
+    client.deactivate();
+}
+
+function sendMessage(client, userName, roomId) {
     var message = {
         value: document.getElementById('message-input').value,
-        user: 'user123' // Zamiast 'user123' możesz wstawić nazwę użytkownika
+        user: userName
     };
     console.log('Wysłano wiadomość:', message);
     client.publish({
-        destination: '/app/chat',
+        destination: `/app/chat/${roomId}`,
         body: JSON.stringify(message),
         headers: {}
     })
@@ -31,7 +46,28 @@ function sendMessage() {
 
 var sendButton = document.getElementById('sendButton');
 sendButton.addEventListener('click', function() {
-    sendMessage();
+    if(client != undefined)
+    sendMessage(client, userName, conversationId);
 });
 
-client.activate();
+var connectButton = document.getElementById('connectButton');
+connectButton.addEventListener('click', function() {
+    conversationId = document.getElementById('conversation-id').value;
+    if (!conversationId.trim()) {
+        console.log("pusty room")
+        return;
+    }
+    var x = connect(conversationId);
+    client = x.client;
+    userName = x.userName;
+    console.log("połączono")
+});
+
+var disconnectButton = document.getElementById('disconnectButton');
+disconnectButton.addEventListener('click', function() {
+    if(client != undefined)
+    disconnect(client);
+    client = undefined
+    console.log("rozłączono")
+});
+
