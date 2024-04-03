5f3870423bdb8ccd0cca06ddf77ceeea20043d86
Sladamos <sladamo@wp.pl>
2024-04-04 01:33:27 +0200

    SLEMA-129 finished simple chat

--- ChatSite/src/app/chat/view/chat-view/chat-view.component.ts
+++ ChatSite/src/app/chat/view/chat-view/chat-view.component.ts
@@ -7,11 +7,13 @@ import { formatTime, formatDate } from '../../../utils/DateFormatUtils';
 import { ChatMessage } from '../../model/chat-message';
 import { ChatService, createChatService } from '../../service/chat.service';
 import { Message as StompMessage } from '@stomp/stompjs';
+import { Subscription } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-view',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule, NgFor, FormsModule],
   templateUrl: './chat-view.component.html',
   styleUrl: './chat-view.component.css',
   providers: [{
@@ -26,16 +28,21 @@ export class ChatViewComponent {
   messages: Messages = {
     messages: []
   };
+  subscribedTopics: Subscription[] = [];
 
   constructor(private service: ChatService, private route: ActivatedRoute, private location: Location) {
   }
 
   ngOnInit(): void {
     this.conversationId = this.route.snapshot.paramMap.get('id') ?? '';
-    this.service.watch({destination: `/user/topic/messages/${this.conversationId}`}).subscribe((message: StompMessage) => {
-      console.log(message);
+    let historyTopic = this.service.watch({destination: `/user/topic/messages/${this.conversationId}`}).subscribe((message: StompMessage) => {
       this.messages = JSON.parse(message.body);
     })
+    this.subscribedTopics.push(historyTopic);
+    let newMessagesTopic = this.service.watch({destination: `/topic/messages/${this.conversationId}`}).subscribe((message: StompMessage) => {
+      this.messages.messages.push(JSON.parse(message.body));
+    })
+    this.subscribedTopics.push(newMessagesTopic);
   }
 
   goBack(): void {
@@ -52,7 +59,10 @@ export class ChatViewComponent {
       content: this.messageContent,
       dateTime: new Date(),
     }
-    console.log(chatMessage);
+    this.service.publish({
+      destination: `/app/chat/${this.conversationId}`,
+      body: JSON.stringify(chatMessage),
+    });
     this.messageContent = ""
   }
 
@@ -64,4 +74,9 @@ export class ChatViewComponent {
     return formatTime(dateTimeStr);
   }
 
+  ngOnDestroy(): void {
+    this.subscribedTopics.forEach(topic => {
+      topic.unsubscribe();
+    });
+  }
 }
