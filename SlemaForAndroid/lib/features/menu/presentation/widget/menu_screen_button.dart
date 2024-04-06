ffe13da02b3041b6b870688ed023c00650c14e70
pikol93 <pikol93@wp.pl>
2024-04-06 07:58:22 +0200

    SLEMA-188 Split menu screen button into multiple files in order to allow to use it in more contexts

--- SlemaForAndroid/lib/features/menu/presentation/widget/menu_screen_button.dart
+++ SlemaForAndroid/lib/features/menu/presentation/widget/menu_screen_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pg_slema/features/well_being/presentation/widget/link_button.dart';
 import 'package:provider/provider.dart';
 import 'package:pg_slema/main/presentation/controller/main_screen_controller.dart';
 
@@ -7,51 +8,22 @@ class MenuScreenButton extends StatelessWidget {
   final String label;
   final int destinationIndex;
 
-  const MenuScreenButton(
-      {super.key,
-      required this.iconData,
-      required this.label,
-      required this.destinationIndex});
+  const MenuScreenButton({
+    super.key,
+    required this.iconData,
+    required this.label,
+    required this.destinationIndex,
+  });
 
   @override
   Widget build(BuildContext context) {
     final controller = Provider.of<MainScreenController>(context);
 
-    return InkWell(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Icon(
-              iconData,
-              size: 40,
-              color: Theme.of(context).primaryColor,
-            ),
-            const SizedBox(
-              width: 10.0,
-            ),
-            Text(label,
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge
-                    ?.copyWith(height: .75)),
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Icon(
-                  Icons.chevron_right_rounded,
-                  size: 40,
-                  color: Theme.of(context).colorScheme.outlineVariant,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      onTap: () {
-        controller.onTabTapped(destinationIndex);
-      },
-    );
+    return LinkButton(
+        iconData: iconData,
+        label: label,
+        onTap: () {
+          controller.onTabTapped(destinationIndex);
+        });
   }
 }
