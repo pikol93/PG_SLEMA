478c482daf1974305d5e97213fb72fec0b2b67d1
Sladamos <sladamo@wp.pl>
2024-04-08 01:31:10 +0200

    SLEMA-187 displaying users list

--- ChatSite/src/app/app.routes.ts
+++ ChatSite/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ConversationListComponent } from './conversation/view/conversation-list/conversation-list.component';
 import { ChatViewComponent } from './chat/view/chat-view/chat-view.component';
+import { UserListComponent } from './user/view/user-list/user-list.component';
 
 export const routes: Routes = [
     {path: 'conversations', component: ConversationListComponent},
-    {path: 'conversations/:id/chat', component: ChatViewComponent}
+    {path: 'conversations/:id/chat', component: ChatViewComponent},
+    {path: 'users', component: UserListComponent}
 ];
