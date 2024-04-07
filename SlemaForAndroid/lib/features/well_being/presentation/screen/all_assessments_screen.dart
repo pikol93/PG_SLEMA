802c57117bd7fae7abd1df2722a26f909bea6842
pikol93 <pikol93@wp.pl>
2024-04-07 12:47:47 +0200

    SLEMA-175 Changed app bar of AllAssessmentsScreen

--- SlemaForAndroid/lib/features/well_being/presentation/screen/all_assessments_screen.dart
+++ SlemaForAndroid/lib/features/well_being/presentation/screen/all_assessments_screen.dart
@@ -6,7 +6,7 @@ import 'package:pg_slema/features/well_being/logic/entity/assessment_factory.dar
 import 'package:pg_slema/features/well_being/logic/service/assessments_service.dart';
 import 'package:pg_slema/features/well_being/presentation/widget/all_assessments/single_assessment_widget.dart';
 import 'package:pg_slema/utils/log/logger_mixin.dart';
-import 'package:pg_slema/utils/widgets/appbars/default_appbar.dart';
+import 'package:pg_slema/utils/widgets/appbars/white_app_bar.dart';
 import 'package:pg_slema/utils/widgets/default_body/default_body.dart';
 
 class AllAssessmentsScreen extends StatefulWidget {
@@ -50,7 +50,8 @@ class _AllAssessmentsScreenState extends State<AllAssessmentsScreen>
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const DefaultAppBar(title: "Raporty zdrowotne"),
+        const SizedBox(height: 20.0),
+        const WhiteAppBar(titleText: "Raporty"),
         DefaultBody(
           child: FutureBuilder(
             future: assessmentsFuture,
