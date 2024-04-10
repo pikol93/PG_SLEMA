40fd7a6103dad66ef644210e3e3df1314caeab48
amddaa <slusarekadam1@gmail.com>
2024-04-10 17:32:17 +0200

    SLEMA-132 Add unfocus parent

--- SlemaForAndroid/lib/features/chat/presentation/screen/thread_chat_screen.dart
+++ SlemaForAndroid/lib/features/chat/presentation/screen/thread_chat_screen.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:pg_slema/utils/widgets/appbars/default_appbar.dart';
 import 'package:pg_slema/utils/widgets/default_body/default_body.dart';
 import 'package:pg_slema/features/chat/presentation/widget/chat_message_input.dart';
+import 'package:pg_slema/utils/widgets/unfocus_on_children_tap.dart';
 
 class ThreadChatScreen extends StatelessWidget {
   const ThreadChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        DefaultAppBar(title: "Nazwa watku TODO"),
-        DefaultBody(child: Text("TODO")),
-        ChatMessageInput(onSendPressed: _onMessageSend),
-      ],
+    return UnfocusOnChildrenTap(
+      child: Column(
+        children: [
+          const DefaultAppBar(title: "Nazwa watku TODO"),
+          const DefaultBody(child: Text("TODO")),
+          ChatMessageInput(onSendPressed: _onMessageSend),
+        ],
+      ),
     );
   }
 
