b86d1cb908546088bb85fbe92d1d124eeb40cc98
pikol93 <piotrdampc93@gmail.com>
2024-06-07 08:20:06 +0200

    SLEMA-136 [WIP] Changed single image widget to not display circular progress indicator

--- SlemaForAndroid/lib/features/gallery/presentation/widget/single_image_widget.dart
+++ SlemaForAndroid/lib/features/gallery/presentation/widget/single_image_widget.dart
@@ -3,7 +3,7 @@ import 'package:pg_slema/features/gallery/logic/entity/image_metadata.dart';
 import 'package:pg_slema/features/gallery/presentation/screen/image_screen.dart';
 import 'package:pg_slema/utils/log/logger_mixin.dart';
 
-class SingleImageWidget extends StatelessWidget with Logger {
+class SingleImageWidget extends StatefulWidget {
   final ImageMetadata metadata;
 
   const SingleImageWidget({
@@ -12,27 +12,52 @@ class SingleImageWidget extends StatelessWidget with Logger {
   });
 
   @override
+  State<SingleImageWidget> createState() => _SingleImageWidgetState();
+}
+
+class _SingleImageWidgetState extends State<SingleImageWidget> with Logger {
+  late Future<void> thumbnailFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: Insert actual future
+    thumbnailFuture = Future.value();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: InkWell(
         onTap: () => _onTapped(context),
-        child: const SizedBox(
+        child: SizedBox(
           width: 50,
           height: 50,
-          child: CircularProgressIndicator(),
+          child: FutureBuilder(
+            future: thumbnailFuture,
+            builder: _futureBuilder,
+          ),
         ),
       ),
     );
   }
 
+  Widget _futureBuilder(BuildContext context, AsyncSnapshot<void> snapshot) {
+    if (snapshot.connectionState != ConnectionState.done) {
+      return const CircularProgressIndicator();
+    }
+
+    return const Text("asd");
+  }
+
   void _onTapped(BuildContext context) {
-    logger.debug("Tapped image ${metadata.filename}");
+    logger.debug("Tapped image ${widget.metadata.filename}");
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => ImageScreen(
-          metadata: metadata,
+          metadata: widget.metadata,
         ),
       ),
     );
