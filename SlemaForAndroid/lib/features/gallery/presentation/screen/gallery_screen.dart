820bf0133f359fc1d2574b1f6e9ecdde366995f4
pikol93 <piotrdampc93@gmail.com>
2024-06-12 21:26:49 +0200

    SLEMA-136 Fixed gallery not refreshing images properly

--- SlemaForAndroid/lib/features/gallery/presentation/screen/gallery_screen.dart
+++ SlemaForAndroid/lib/features/gallery/presentation/screen/gallery_screen.dart
@@ -60,6 +60,10 @@ class GalleryScreenState extends State<GalleryScreen> with Logger {
     return FutureBuilder(
       future: imageMetadataFuture,
       builder: (context, result) {
+        if (result.connectionState != ConnectionState.done) {
+          return const CircularProgressIndicator();
+        }
+        
         if (result.hasError) {
           logger.error("Returned result has an error: ${result.error}");
           return _buildError();
